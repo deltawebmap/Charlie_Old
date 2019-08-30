d9b47a1aa88cad7ace9aecd52a4c81df3f878771
Roman Port <34401219+Roman-Port@users.noreply.github.com>
2019-08-30 17:15:25 -0500

    Add project files.

---
+++ DeltaDataExtractor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeltaDataExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
